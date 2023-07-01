0818fd83d5e66f9a4c025e69843190c27bd45d96
Vedant Arora <93697157+VedantArora18@users.noreply.github.com>
2023-07-01 13:29:51 +0530

    tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -7,9 +7,7 @@ board_keys = []
 for key in theBoard:
     board_keys.append(key)
 
-''' We will have to print the updated board after every move in the game and 
-    thus we will make a function in which we'll define the printBoard function
-    so that we can easily print the board everytime by calling this function. '''
+
 
 def printBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
@@ -18,7 +16,7 @@ def printBoard(board):
     print('-+-+-')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
 
-# Now we'll write the main function which has all the gameplay functionality.
+
 def game():
 
     turn = 'X'
@@ -38,7 +36,7 @@ def game():
             print("That place is already filled.\nMove to which place?")
             continue
 
-        # Now we will check if player X or O has won,for every move after 5 moves. 
+         
         if count >= 5:
             if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
                 printBoard(theBoard)
@@ -81,18 +79,18 @@ def game():
                 print(" **** " +turn + " won. ****")
                 break 
 
-        # If neither X nor O wins and the board is full, we'll declare the result as 'tie'.
+        
         if count == 9:
             print("\nGame Over.\n")                
             print("It's a Tie!!")
 
-        # Now we have to change the player after every move.
+       
         if turn =='X':
             turn = 'O'
         else:
             turn = 'X'        
     
-    # Now we will ask if player wants to restart the game or not.
+    
     restart = input("Do want to play Again?(y/n)")
     if restart == "y" or restart == "Y":  
         for key in board_keys:
